dfd230187bcb6b432bb25c9455dbd5160975fe73
v.fateev <v.fateev@nexters.com>
2025-05-18 12:31:25 +0300

    feat(models): replace WeatherForecast with Stock and Comment models
    Replace the placeholder WeatherForecast setup with Stock and Comment models to better align with the application's financial domain. This change introduces new model classes for managing stock and comment data, along with Entity Framework annotations for database mapping. It also updates the project structure accordingly.

---
+++ FinShark.API/Models/Comment.cs
@@ -0,0 +1,13 @@
+namespace FinShark.API.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
+    // by convention EntityFrameworkCore will use the name of the class as the table name
+    public int? StockId { get; set; }
+    // Navigation property
+    public Stock? Stock { get; set; }
+}
