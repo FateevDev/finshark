fb580df72d000d6efe2c02962ee1bdf29482a6ec
v.fateev <v.fateev@nexters.com>
2025-05-21 19:34:28 +0300

    feat(makefile): add formatting check and fix commands
    Add `dotnet format` commands to verify and fix code formatting issues in the Makefile. Improved code structure in `Program.cs` with varied examples of variable types and operations for testing purposes.

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ up:
 	docker compose up -d
 down:
 	docker compose down
+check:
+	dotnet format --verify-no-changes
+check-fix:
+	dotnet format
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,5 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 var helloWorld = "Hello World";
+var test = """
+           some text
+           another string           
+           """;
+int a = 123;
+float b = 12345678910.123456789f;
+decimal c = 12345678910.123456789m;
+long d = 1234567891012345678;
+
 Console.WriteLine(helloWorld);
-Console.WriteLine(helloWorld);
+Console.WriteLine(8 / 2 + 5 - -3 / 2);
+// Console.WriteLine(c);
+// var readLine = Console.ReadLine();
+// var consoleKeyInfo = Console.ReadKey(true);
+// Console.WriteLine("Input: " + readLine);
+// Console.WriteLine("Key: " + consoleKeyInfo.KeyChar);
