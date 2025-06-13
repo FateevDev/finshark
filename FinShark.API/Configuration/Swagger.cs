23f6ec06ca47f5d38a0761658d5f134b346ae5b2
v.fateev <v.fateev@nexters.com>
2025-06-13 21:03:18 +0300

    feat(swagger): add JWT auth support to Swagger config
    Enhanced Swagger configuration by adding a `Bearer` security scheme and requirement for JWT authentication. This improves API documentation by enabling token-based authentication integration, allowing developers to test secured endpoints directly within Swagger UI.

--- FinShark.API/Configuration/Swagger.cs
+++ FinShark.API/Configuration/Swagger.cs
@@ -1,4 +1,5 @@
 using FinShark.API.Swagger.Examples;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 
 namespace FinShark.API.Configuration;
@@ -7,14 +8,38 @@ public static class Swagger
 {
     public static WebApplicationBuilder AddSwagger(this WebApplicationBuilder builder)
     {
-        builder.Services.AddSwaggerGen(config =>
+        builder.Services.AddSwaggerGen(options =>
         {
-            config.EnableAnnotations();
-            config.ExampleFilters();
+            options.EnableAnnotations();
+            options.ExampleFilters();
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    []
+                }
+            });
         });
 
         builder.Services.AddSwaggerExamplesFromAssemblyOf<UserLoginDtoExample>();
-
 
         return builder;
     }
