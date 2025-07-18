f05099d833c2185f2812ff9a97bfd442b4569e4f
v.fateev <v.fateev@nexters.com>
2025-07-18 19:28:03 +0300

    feat(tests): add API test project with initial test case
    Added the `FinShark.API.Tests` project to the solution to accommodate tests for the API layer. Included a placeholder test case for `PortfolioController` and configured the project to target .NET 8.0 with necessary test dependencies. This improves the project's test structure and prepares for enhanced API test coverage.

---
+++ FinShark.API.Tests/Controllers/v1/PortfolioControllerTest.cs
@@ -0,0 +1,16 @@
+using FinShark.API.Controllers.v1;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace FinShark.API.Tests.Controllers.v1;
+
+[TestSubject(typeof(PortfolioController))]
+public class PortfolioControllerTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
