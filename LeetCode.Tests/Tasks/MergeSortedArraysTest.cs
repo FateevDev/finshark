66c175113794e23808681fa85274a4466487bf14
v.fateev <v.fateev@nexters.com>
2025-07-19 21:43:26 +0300

    feat: add LeetCode project

---
+++ LeetCode.Tests/Tasks/MergeSortedArraysTest.cs
@@ -0,0 +1,15 @@
+using JetBrains.Annotations;
+using LeetCode.Tasks;
+
+namespace LeetCode.Tests.Tasks;
+
+[TestSubject(typeof(MergeSortedArrays))]
+public class MergeSortedArraysTest
+{
+
+    [Xunit.Fact]
+    public void METHOD()
+    {
+        
+    }
+}
---
+++ LeetCode/Tasks/MergeSortedArrays.cs
@@ -0,0 +1,23 @@
+namespace LeetCode.Tasks;
+
+// Merge Sorted Array
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+// and two integers m and n, representing the number of elements in nums1 and nums2.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+// To accommodate this, nums1 has a length of m + n,
+// where the first m elements denote the elements that should be merged,
+// and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+// Объединить отсортированные массивы
+// Даны два целочисленных массива nums1 и nums2, отсортированные по возрастанию, и два числа m и n,
+// которые обозначают количество элементов в nums1 и nums2 соответственно.
+// Необходимо объединить nums1 и nums2 в один массив, отсортированный по возрастанию.
+// Итоговый отсортированный массив не должен возвращаться из функции,
+// а должен быть записан прямо в массив nums1.
+// Для этого nums1 имеет длину m + n, где первые m элементов — это данные, которые нужно объединить,
+// а последние n элементов равны 0 и должны быть проигнорированы. nums2 имеет длину n.
+public class MergeSortedArrays
+{
+
+}
