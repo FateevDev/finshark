a4013b9779a622e9d9624ca400d0ca08b2bfdc82
v.fateev <v.fateev@nexters.com>
2025-08-13 22:24:13 +0300

    feat(tasks): scaffold solution for Lowest Common Ancestor in BST
    Set up the initial structure for solving the "Lowest Common Ancestor of a Binary Search Tree" problem. Added the `LowestCommonAncestor` method and its corresponding test class to start the implementation and validation process.

---
+++ LeetCode.Tests/Tasks/LowestCommonAncestorOfABinarySearchTreeTest.cs
@@ -0,0 +1,55 @@
+using JetBrains.Annotations;
+using LeetCode.Tasks;
+
+namespace LeetCode.Tests.Tasks;
+
+[TestSubject(typeof(LowestCommonAncestorOfABinarySearchTree))]
+public class LowestCommonAncestorOfABinarySearchTreeTest
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void LowestCommonAncestor_WhenCalled_ReturnsExpectedResult(
+        TreeNode root,
+        TreeNode p,
+        TreeNode q,
+        TreeNode expected
+    )
+    {
+        var sut = new LowestCommonAncestorOfABinarySearchTree();
+
+        var lowestCommonAncestor = sut.LowestCommonAncestor(root, p, q);
+
+        Assert.Equal(expected.val, lowestCommonAncestor.val);
+    }
+
+    public static TheoryData<TreeNode, TreeNode, TreeNode, TreeNode> TestData()
+    {
+        return new TheoryData<TreeNode, TreeNode, TreeNode, TreeNode>
+        {
+            {
+                new TreeNode(6,
+                    new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3), new TreeNode(5))),
+                    new TreeNode(8, new TreeNode(7), new TreeNode(8))
+                ),
+                new TreeNode(2),
+                new TreeNode(8),
+                new TreeNode(6)
+            },
+            {
+                new TreeNode(6,
+                    new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3), new TreeNode(5))),
+                    new TreeNode(8, new TreeNode(7), new TreeNode(8))
+                ),
+                new TreeNode(2),
+                new TreeNode(4),
+                new TreeNode(2)
+            },
+            {
+                new TreeNode(2, new TreeNode(1)),
+                new TreeNode(2),
+                new TreeNode(1),
+                new TreeNode(2)
+            },
+        };
+    }
+}
---
+++ LeetCode/Tasks/LowestCommonAncestorOfABinarySearchTree.cs
@@ -0,0 +1,46 @@
+namespace LeetCode.Tasks;
+
+/*
+ * Lowest Common Ancestor of a Binary Search Tree
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+ *
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+ * According to the definition of LCA on Wikipedia:
+ * “The lowest common ancestor is defined between two nodes p and q as the lowest node in T
+ * that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ *
+ * Наименьший общий предок в Двоичном Дереве Поиска
+ * Дано BST, найти наименьшего общего предка 2х данных нод
+ *
+ * Наименьший общий предок определяется между двумя нодами p и q как наименьшая нода в дереве,
+ * которая имеет оба p и q как потомков (где мы позволяем ноде быть потомком себя)
+ *
+ */
+public class LowestCommonAncestorOfABinarySearchTree
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        var leftNode = p.val < q.val ? p : q;
+        var rightNode = leftNode == p ? q : p;
+
+        if (
+            (root.left?.val == leftNode.val && root.right?.val == rightNode.val)
+            || (root.val == leftNode.val && root.right?.val == rightNode.val)
+            || (root.val == rightNode.val && root.left?.val == leftNode.val)
+        )
+        {
+            return root;
+        }
+
+        var nextNode = p.val > root.val && q.val > root.val ? root.right : root.left;
+
+        return LowestCommonAncestor(nextNode!, p, q);
+    }
+}
+
+public class TreeNode(int x, TreeNode? left = null, TreeNode? right = null)
+{
+    public int val = x;
+    public TreeNode? left = left;
+    public TreeNode? right = right;
+}
